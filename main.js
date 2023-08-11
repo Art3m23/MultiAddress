72149722c4c17498b3d6920c8c71f5f5efd0bc10
Art3m23 <76261475+Art3m23@users.noreply.github.com>
2023-08-11 14:56:38 +0300

    Replace files

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const addAddressBtn = document.querySelector('.btn-add');
 const closeBtn = document.querySelector('.close');
 const modalAddress = document.querySelector('.modal_address');
 const row = document.querySelector('.row');
-const form = document.querySelector('.new-address');
-const cards = JSON.parse(localStorage.getItem('cards')) ? JSON.parse(localStorage.getItem('cards')) : [];
+const formAdd = document.querySelector('.new-address');
+const formChange = document.querySelector('.change-address');
+let cardsArray = JSON.parse(localStorage.getItem('cards')) ? JSON.parse(localStorage.getItem('cards')) : [];
 const modalRemove = document.querySelector('.modal_remove');
+const modalChange = document.querySelector('.modal_change');
 const closeRemoveBtn = document.querySelector('.close-remove');
+const closeChangeBtn = document.querySelector('.close-change');
+const cancelChangeBtn = document.querySelector('.btn-cancel-change');
 const btnOK = document.querySelector('.btn-ok');
 const btnCancel = document.querySelector('.btn-cancel');
+let removeIndex, changeIndex = 0;
 
 window.onload = () => {
   if (JSON.parse(localStorage.getItem('cards'))) {
@@ -24,7 +29,7 @@ function addAddress() {
 
 function close() {
   modalAddress.style.display = 'none';
-  form.reset();
+  formAdd.reset();
 };
 
 
@@ -39,24 +44,70 @@ function closeRemove() {
 btnCancel.addEventListener('click', closeRemove);
 
 row.addEventListener('click', function (event) {
-  if(event.target.closest('.btn-remove')){
+  const removeBtns = document.querySelectorAll('.btn-remove');
+  if (event.target.closest('.btn-remove')) {
+    console.log(event.target.closest('.btn-remove'));
+    removeBtns.forEach((el, i) => {
+      if (el === event.target.closest('.btn-remove')) {
+        removeIndex = i;
+        console.log(i);
+      }
+    });
     modalRemove.style.display = 'block';
   } else {
     return;
   }
 });
-closeRemoveBtn.addEventListener('click', closeRemove);
 
+closeRemoveBtn.addEventListener('click', closeRemove);
+cancelChangeBtn.addEventListener('click', closeChange);
+
+row.addEventListener('click', function (event) {
+  let arrValueForm = [];
+  const cardTitle = event.target.closest('.card').querySelector('.card-title');
+  const cardSubTitle = event.target.closest('.card').querySelector('.card-subtitle');
+  const inputsFormChange = document.querySelectorAll('.change-address-input');
+  arrValueForm[5] = cardTitle.innerHTML.split(';')[0];
+  arrValueForm[6] = cardTitle.innerHTML.split(';')[1];
+  arrValueForm[3] = cardSubTitle.innerHTML.split(';')[0];
+  arrValueForm[2] = cardSubTitle.innerHTML.split(';')[1];
+  arrValueForm[1] = cardSubTitle.innerHTML.split(';')[2];
+  arrValueForm[0] = cardSubTitle.innerHTML.split(';')[3];
+  arrValueForm[4] = cardSubTitle.innerHTML.split(';')[4];
+  const changeBtns = document.querySelectorAll('.btn-change');
+  if (event.target.closest('.btn-change')) {
+    console.log(event.target.closest('.btn-change'));
+    changeBtns.forEach((el, i) => {
+      if (el === event.target.closest('.btn-change')) {
+        changeIndex = i;
+        console.log(i);
+      }
+    });
+    modalChange.style.display = 'block';
+  } else {
+    return;
+  }
+  console.log(arrValueForm);
+  inputsFormChange.forEach((el, i) => {
+    el.value = arrValueForm[i];
+  });
+});
+
+function closeChange() {
+  modalChange.style.display = 'none';
+};
+
+closeChangeBtn.addEventListener('click', closeChange);
 
 function submit(e) {
   e.preventDefault();
   const htmlElement = `<div class="col d-flex  justify-content-center">
     <div class="card">
       <div class="card-body d-flex  justify-content-between flex-column">
-        <h5 class="card-title mb-4 ps-5">${e.currentTarget[5].value + ',' + e.currentTarget[6].value}</h5>
-        <h6 class="card-subtitle mb-4 ps-5">${e.currentTarget[3].value + ',' + e.currentTarget[2].value + ',' + e.currentTarget[1].value + ',' + e.currentTarget[0].value + ',' + e.currentTarget[4].value}</h6>
+        <h5 class="card-title mb-4 ps-5">${e.currentTarget[5].value + '; ' + e.currentTarget[6].value}</h5>
+        <h6 class="card-subtitle mb-4 ps-5">${e.currentTarget[3].value + '; ' + e.currentTarget[2].value + '; ' + e.currentTarget[1].value + '; ' + e.currentTarget[0].value + '; ' + e.currentTarget[4].value}</h6>
         <div class="container-btn d-flex justify-content-start">
-          <a href="#" class="card-link">
+          <a href="#change-address" class="card-link">
             <button type="submit" class="btn-card btn-change">Изменить</button>
           </a>
           <a href="#remove-address" class="card-link">
@@ -67,17 +118,55 @@ function submit(e) {
     </div>
   </div>`;
   row.innerHTML += htmlElement;
-  cards.push(htmlElement);
-  localStorage.setItem('cards', JSON.stringify(cards));
-  form.reset();
+  cardsArray.push(htmlElement);
+  localStorage.setItem('cards', JSON.stringify(cardsArray));
+  formAdd.reset();
   modalAddress.style.display = 'none';
-}
+  document.location.href = document.location.href.split('#')[0] + '#';
+};
 
-form.addEventListener('submit', submit)
+formAdd.addEventListener('submit', submit);
 
 
 function remove() {
-  
+  row.innerHTML = '';
+  cardsArray = cardsArray.filter((el, i) => i !== removeIndex);
+  cardsArray.forEach(element => {
+    row.innerHTML += element;
+  });
+  localStorage.setItem('cards', JSON.stringify(cardsArray));
+  modalRemove.style.display = 'none';
 };
 
 btnOK.addEventListener('click', remove);
+
+function change(e) {
+  e.preventDefault();
+  const htmlElement = `<div class="col d-flex  justify-content-center">
+    <div class="card">
+      <div class="card-body d-flex  justify-content-between flex-column">
+        <h5 class="card-title mb-4 ps-5">${e.currentTarget[5].value + '; ' + e.currentTarget[6].value}</h5>
+        <h6 class="card-subtitle mb-4 ps-5">${e.currentTarget[3].value + '; ' + e.currentTarget[2].value + '; ' + e.currentTarget[1].value + '; ' + e.currentTarget[0].value + '; ' + e.currentTarget[4].value}</h6>
+        <div class="container-btn d-flex justify-content-start">
+          <a href="#change-address" class="card-link">
+            <button type="submit" class="btn-card btn-change">Изменить</button>
+          </a>
+          <a href="#remove-address" class="card-link">
+            <button type="submit" class="btn-card btn-remove" >Удалить</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  row.innerHTML = '';
+  cardsArray[changeIndex] = htmlElement;
+  cardsArray.forEach(element => {
+    row.innerHTML += element;
+  });
+  localStorage.setItem('cards', JSON.stringify(cardsArray));
+  formChange.reset();
+  modalChange.style.display = 'none';
+  document.location.href = document.location.href.split('#')[0] + '#';
+};
+
+formChange.addEventListener('submit', change);
